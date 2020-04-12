d4e9b11ae334a583f1952d3b37833c0bab5b225e
subta90 <the.bird.nobility298@gmail.com>
2020-04-12 16:52:27 +0900

    add to open Url

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.androidfundamentals
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mWebsiteEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mWebsiteEditText = findViewById(R.id.website_edittext)
     }
 
-    fun openWebsite(view: View) {}
+    fun openWebsite(view: View) {
+        val urlString = mWebsiteEditText.text.toString()
+        val url = Uri.parse(urlString)
+
+        val intent = Intent(Intent.ACTION_VIEW, url)
+        intent.resolveActivity(packageManager).let {
+            startActivity(intent)
+        }
+
+    }
+
     fun openLocation(view: View) {}
+
     fun shareText(view: View) {}
 }
