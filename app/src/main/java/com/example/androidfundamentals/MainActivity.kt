f093ff1b0adf4b59d8d8a67e1016957003a5f301
subta90 <the.bird.nobility298@gmail.com>
2020-04-12 17:01:25 +0900

    add to shareText

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -6,17 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.core.app.ShareCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mWebsiteEditText: EditText
     private lateinit var mLocationEditText: EditText
+    private lateinit var mShareTextEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mWebsiteEditText = findViewById(R.id.website_edittext)
         mLocationEditText = findViewById(R.id.location_edittext)
+        mShareTextEditText = findViewById(R.id.share_edittext)
     }
 
     fun openWebsite(view: View) {
@@ -39,5 +42,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun shareText(view: View) {}
+    fun shareText(view: View) {
+        val shareTextString = mShareTextEditText.text.toString()
+        val mimeType = "text/plain"
+
+        ShareCompat.IntentBuilder
+            .from(this)
+            .setType(mimeType)
+            .setChooserTitle("Share this text with: ")
+            .setText(shareTextString)
+            .startChooser()
+    }
 }
