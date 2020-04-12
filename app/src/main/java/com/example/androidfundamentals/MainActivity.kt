1ecc06c8c3fc1275105d7d255d84bf529d88ba3b
subta90 <the.bird.nobility298@gmail.com>
2020-04-12 15:29:55 +0900

    add to reply intent

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -1,25 +1,32 @@
 package com.example.androidfundamentals
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         const val extraMessage = "com.example.android.androidfundamentals.extra.MESSAGE"
+        const val textRequest = 1
     }
 
     private val logTag = MainActivity::class.java.simpleName
     private lateinit var mMessageEditText: EditText
+    private lateinit var mReplyHeaderTextView: TextView
+    private lateinit var mReplyTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mMessageEditText = findViewById(R.id.editText_main)
+        mReplyHeaderTextView = findViewById(R.id.text_header_reply)
+        mReplyTextView = findViewById(R.id.text_message_reply)
     }
 
     fun launchSecondActivity(view: View) {
@@ -28,6 +35,24 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SecondActivity::class.java)
         val message = mMessageEditText.text.toString()
         intent.putExtra(extraMessage, message)
-        startActivity(intent)
+        startActivityForResult(intent, textRequest)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            textRequest -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    val reply = data?.getStringExtra(SecondActivity.extraReply)
+                    mReplyHeaderTextView.visibility = View.VISIBLE
+                    mReplyTextView.visibility = View.VISIBLE
+                    mReplyTextView.text = reply
+                }
+            }
+            else -> {
+
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/androidfundamentals/SecondActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/SecondActivity.kt
@@ -1,10 +1,19 @@
 package com.example.androidfundamentals
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+
+    companion object {
+        const val extraReply = "com.example.android.androidfundamentals.extra.REPLY"
+    }
+
+    private lateinit var mReply: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +21,16 @@ class SecondActivity : AppCompatActivity() {
         val message = intent.getStringExtra(MainActivity.extraMessage)
         val textView = findViewById<TextView>(R.id.text_message)
         textView.text = message
+
+        mReply = findViewById(R.id.editText_second)
+    }
+
+    fun returnReply(view: View) {
+        val reply = mReply.text.toString()
+
+        val replyIntent = Intent()
+        replyIntent.putExtra(extraReply, reply)
+        setResult(RESULT_OK, replyIntent)
+        finish()
     }
 }
