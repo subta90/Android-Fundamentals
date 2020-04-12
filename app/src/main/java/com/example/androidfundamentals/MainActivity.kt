ccac1d441642d9092e80a2a7c2007d41170e656a
subta90 <the.bird.nobility298@gmail.com>
2020-04-12 13:05:31 +0900

    add to send button

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchSecondActivity(view: View) {
+        Log.d(LOG_TAG, "Button Clicked!")
+
     }
 }
