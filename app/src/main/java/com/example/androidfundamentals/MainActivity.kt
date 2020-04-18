096a828a2e933433a5a943d181062f36c3adc856
subta90 <the.bird.nobility298@gmail.com>
2020-04-18 17:10:55 +0900

    add to click action

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun displayToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Shows a message that the donut image was clicked.
+     */
+    fun showDonutOrder(view: View) {
+        displayToast(getString(R.string.dount_order_message))
+    }
+
+    /**
+     * Shows a message that the ice cream sandwich image was clicked.
+     */
+    fun showIceCreamOrder(view: View) {
+        displayToast(getString(R.string.ice_cream_order_message))
+    }
+
+    /**
+     * Shows a message that the froyo image was clicked.
+     */
+    fun showFroyoOrder(view: View) {
+        displayToast(getString(R.string.froyo_order_message))
+    }
+
 }
