44d5138e66625d746c0ded144e098f4cb69f5038
subta90 <the.bird.nobility298@gmail.com>
2020-04-19 14:56:01 +0900

    Add to radiogroup.

--- app/src/main/java/com/example/androidfundamentals/OrderActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/OrderActivity.kt
@@ -2,7 +2,10 @@ package com.example.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
 import android.widget.TextView
+import android.widget.Toast
 
 class OrderActivity : AppCompatActivity() {
 
@@ -14,5 +17,32 @@ class OrderActivity : AppCompatActivity() {
         val message = "Order: " + intent.getStringExtra(MainActivity.extraMessage)
         val textView = findViewById<TextView>(R.id.order_textview)
         textView.text = message
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view !is RadioButton) return
+        val checked = view.isChecked
+
+        when (view.id) {
+            R.id.sameday ->
+                if (checked) {
+                    displayToast(getString(R.string.same_day_messenger_service))
+                }
+
+            R.id.nextday ->
+                if (checked) {
+                    displayToast(getString(R.string.next_day_ground_delivery))
+                }
+
+            R.id.pickup ->
+                if (checked) {
+                    displayToast(getString(R.string.pick_up))
+                }
+
+        }
+    }
+
+    fun displayToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
 }
