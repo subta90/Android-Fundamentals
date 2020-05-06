e39192aa26d336ead3e34fe6a94949e0670f5474
subta90 <the.bird.nobility298@gmail.com>
2020-05-06 14:45:44 +0900

    Create a more useful detail page

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
@@ -17,12 +17,10 @@
 package com.example.android.marsrealestate.detail
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.example.android.marsrealestate.detail.DetailFragment
 import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.R
 
 /**
  * The [ViewModel] that is associated with the [DetailFragment].
@@ -37,4 +35,21 @@ class DetailViewModel(marsProperty: MarsProperty, app: Application) : AndroidVie
         _selectedProperty.value = marsProperty
     }
 
+    val displayPropertyPrice = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(
+                when(it.isRental) {
+                    true -> R.string.display_price_monthly_rental
+                    false -> R.string.display_price
+                }, it.price)
+    }
+
+    val displayPropertyType = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(R.string.display_type,
+        app.applicationContext.getString(
+                when(it.isRental) {
+                    true -> R.string.type_rent
+                    false -> R.string.type_sale
+                }
+        ))
+    }
 }
