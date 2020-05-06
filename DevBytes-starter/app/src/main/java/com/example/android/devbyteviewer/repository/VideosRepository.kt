bc7123ca38412a87e5e1f0a87316574329162d48
subta90 <the.bird.nobility298@gmail.com>
2020-05-06 16:05:49 +0900

    Integrate the repository using a refresh strategy

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -27,7 +27,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 
-class VideoRepository(private val database: VideosDatabase) {
+class VideosRepository(private val database: VideosDatabase) {
 
     val videos: LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()) {
         it.asDomainModel()
