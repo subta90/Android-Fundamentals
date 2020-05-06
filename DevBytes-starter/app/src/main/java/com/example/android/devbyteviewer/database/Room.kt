d204a45b962ca61cfa7cdd25e92bc103cce9aec8
subta90 <the.bird.nobility298@gmail.com>
2020-05-06 15:27:57 +0900

    Add an offline cache

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -15,3 +15,40 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface VideoDao {
+
+    @Query("select * from databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(videos: List<DatabaseVideo>)
+
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase: RoomDatabase() {
+    abstract val videoDao: VideoDao
+
+    init {
+        videoDao.getVideos()
+    }
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+            VideosDatabase::class.java,
+            "videos").build()
+        }
+    }
+    return INSTANCE
+}
